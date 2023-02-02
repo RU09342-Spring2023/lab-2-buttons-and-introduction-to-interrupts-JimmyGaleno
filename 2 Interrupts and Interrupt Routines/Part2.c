a5dec04deccac768b6654ec0147cf3469ba482e2
James Galeno <galeno53@students.rowan.edu>
2023-02-02 16:44:37 -0500

    Added green led

--- 2 Interrupts and Interrupt Routines/Part2.c
+++ 2 Interrupts and Interrupt Routines/Part2.c
@@ -27,6 +27,9 @@ int main(void)
     P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
     P1DIR |= BIT0;                          // Set P1.0 to output direction
 
+    P6OUT &= ~BIT6;                         // Clear P6.6 output latch for a defined power-on state
+    P6DIR |= BIT6;                          // Set P6.6 to output direction
+
     // @TODO You need to add in the configuration for the Green LED
 
     P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
