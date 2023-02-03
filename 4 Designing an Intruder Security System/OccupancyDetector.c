2ef32b2cfd5fd905cf3c7e715520d35cebfc9432
James Galeno <galeno53@students.rowan.edu>
2023-02-02 20:27:59 -0500

    green blinks with no button/ red blinks with button

--- 4 Designing an Intruder Security System/OccupancyDetector.c
+++ 4 Designing an Intruder Security System/OccupancyDetector.c
@@ -1,10 +1,54 @@
 /*
  * OccupancyDetector.c
  *
- *  Created on: Jan 30, 2023
- *      Author: russty
+ *  Created on: 2/2/2023
+ *      Author: Jimmy Galeno
  */
 
+#include <msp430.h>
 
+void gpioInit();
+
+int main(void){
+
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+
+    gpioInit();                 // Initialize all GPIO Pins for the project
+
+
+    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
+                                            // to activate previously configured port settings
+
+
+
+    while(1){
+
+        if(~P4IN & BIT1) {                 // When button held red LED blinks and green LED is off
+            P1OUT ^= BIT0;
+            P6OUT &= ~BIT6;
+
+        }
+        if(P4IN & BIT1) {               // When button not held green LED blinks and LED is off
+            P6OUT ^= BIT6;
+            P1OUT &= ~BIT0;
+        }
+
+        __delay_cycles(1000000);             // Delay for 1000000*(1/MCLK)=?
+    }
+
+}
+
+void gpioInit()
+{
+   // Setting Directions of Pins
+
+       P1DIR |= BIT0;              // Configure P1.0 to an Output
+       P6DIR |= BIT6;              // Configure P6.6 to an Output
+       P4DIR &= ~BIT1;             // Configure P4.1 to an Input
+
+       P4REN |= BIT1;               // Enable Resistor on P4.1
+       P4OUT |= BIT1;               // Configure Resistor on P4.1 to be Pullup
+
+}
 
 
