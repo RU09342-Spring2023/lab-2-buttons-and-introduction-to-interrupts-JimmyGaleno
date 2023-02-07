ecc440a43c9cfb84c4fb15f7597d58839cef72a3
James Galeno <galeno53@students.rowan.edu>
2023-02-07 17:38:21 -0500

    green blinks, button held red blinks, after 10 seconds red goes solid

--- 4 Designing an Intruder Security System/OccupancyDetector.c
+++ 4 Designing an Intruder Security System/OccupancyDetector.c
@@ -7,35 +7,55 @@
 
 #include <msp430.h>
 
+
 void gpioInit();
 
 int main(void){
 
-    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;               // stop watchdog timer
 
-    gpioInit();                 // Initialize all GPIO Pins for the project
+    gpioInit();                             // Initialize all GPIO Pins for the project
 
 
     PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
                                             // to activate previously configured port settings
 
+    int count_1 = 0;                        // count_1 is 0
+
 
 
     while(1){
 
-        if(~P4IN & BIT1) {                 // When button held red LED blinks and green LED is off
+        if(~P4IN & BIT1) {                  // When button held red LED blinks and green LED is off
             P1OUT ^= BIT0;
             P6OUT &= ~BIT6;
 
+            __delay_cycles(500000);         // Delay for 500000*(1/MCLK)= around .5 s
+
+
+            count_1++;
+
+            if(count_1 == 10){                // count reaches 10 sec, solid red LED
+                P1OUT ^= BIT0;
+
+                count_1 = 0;
+                break;
+
+            }
+
+
         }
-        if(P4IN & BIT1) {               // When button not held green LED blinks and LED is off
+
+
+        if(P4IN & BIT1) {                   // When button not held green LED blinks and red LED is off
             P6OUT ^= BIT6;
             P1OUT &= ~BIT0;
+
+            __delay_cycles(3000000);        // Delay for 3000000*(1/MCLK)= 3 s
         }
 
-        __delay_cycles(1000000);             // Delay for 1000000*(1/MCLK)=?
-    }
 
+    }
 }
 
 void gpioInit()
