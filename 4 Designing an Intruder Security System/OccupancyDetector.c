6f0c777db216f46937cc09e1c25e93ecf27df02c
James Galeno <galeno53@students.rowan.edu>
2023-02-08 19:32:04 -0500

    need document (moved other code into case given to get reset to work)

--- 4 Designing an Intruder Security System/OccupancyDetector.c
+++ 4 Designing an Intruder Security System/OccupancyDetector.c
@@ -6,6 +6,9 @@
  */
 
 #include <msp430.h>
+#define armed 0                             // case when green LED blinks
+#define warning 1                           // case when red LED blinks
+#define alert 2                             // case when red LED is on
 
 
 void gpioInit();
@@ -22,41 +25,67 @@ int main(void){
 
     int count_1 = 0;                        // count_1 is 0
 
+    char state = armed;
+
+
 
 
     while(1){
 
-        if(~P4IN & BIT1) {                  // When button held red LED blinks and green LED is off
-            P1OUT ^= BIT0;
-            P6OUT &= ~BIT6;
+        switch (state) {
 
-            __delay_cycles(500000);         // Delay for 500000*(1/MCLK)= around .5 s
+        case armed:
 
+            if(P4IN & BIT1) {                   // When button not held green LED blinks and red LED is off
+                P6OUT ^= BIT6;
+                P1OUT &= ~BIT0;
 
-            count_1++;
+                __delay_cycles(3000000);        // Delay for 3000000*(1/MCLK)= 3 s
 
-            if(count_1 == 10){                // count reaches 10 sec, solid red LED
-                P1OUT ^= BIT0;
-
-                count_1 = 0;
-                break;
-
+                count_1 = 0;                    // count reset to 0
+               }
+            else{
+                state = warning;                // if not move to next case
             }
 
 
-        }
+        break;
+
+        case warning:
+
+            if((~P4IN & BIT1)){                  // When button is held red LED blinks and green LED is off
+                P1OUT ^= BIT0;
+                P6OUT &= ~BIT6;
+
+                __delay_cycles(100000);         // Delay for 100000*(1/MCLK)= around .5 s
 
 
-        if(P4IN & BIT1) {                   // When button not held green LED blinks and red LED is off
-            P6OUT ^= BIT6;
-            P1OUT &= ~BIT0;
+                count_1++;                      // count starts incrementing
+            }
+            if((~P4IN & BIT1) && count_1 == 20){                // When button is pressed and count = 20 move to alert case
+                state = alert;
+            }
+            if(P4IN & BIT1){                                    // When button is not pressed go back to the armed case
+                state = armed;
+            }
 
-            __delay_cycles(3000000);        // Delay for 3000000*(1/MCLK)= 3 s
-        }
+            break;
 
+        case alert:
 
-    }
+            if(~P2IN & BIT3){                   // if button 2.3 is pressed move to armed case
+                state = armed;
+            }
+            else {
+                P1OUT ^= BIT0;                  // red LED stays on while green LED is off
+            }
+            break;
+     }
+   }
 }
+
+
+
 
 void gpioInit()
 {
@@ -65,9 +94,12 @@ void gpioInit()
        P1DIR |= BIT0;              // Configure P1.0 to an Output
        P6DIR |= BIT6;              // Configure P6.6 to an Output
        P4DIR &= ~BIT1;             // Configure P4.1 to an Input
+       P2DIR &= ~BIT3;             // Configure P2.3 to an Input
 
        P4REN |= BIT1;               // Enable Resistor on P4.1
        P4OUT |= BIT1;               // Configure Resistor on P4.1 to be Pullup
+       P2REN |= BIT3;               // Enable Resistor on P2.3
+       P2OUT |= BIT3;               // Configure Resistor on P2.3 to be Pullup
 
 }
 
